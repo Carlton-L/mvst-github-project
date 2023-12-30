77ee5b8547ce76a87b36412f4c8c914f8806185e
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2023-12-31 00:42:27 +0100

    Header and footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
-const Header = () => {
-  return <footer>Footer</footer>;
+const Footer = () => {
+  return (
+    <footer className="border-t border-white p-4 flex justify-between min-h-28">
+      Footer
+    </footer>
+  );
 };
 
-export default Header;
+export default Footer;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Root = () => {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
+    <div className="flex flex-col justify-between min-h-screen bg-black text-white">
       <Header />
       <Outlet />
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 };
