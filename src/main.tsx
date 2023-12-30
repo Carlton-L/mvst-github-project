e5eb70b728de1d123c502e0afcbb10abca463db7
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2023-12-30 22:43:25 +0100

    create-react-app

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import App from "./App.tsx";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
