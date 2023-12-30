86cee1188095a75d6c65bdbced70d90433d14672
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2023-12-30 23:25:47 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     <>
       <h1>Github User Browser</h1>
       <h2>Find a user and browse through their repositories</h2>
+      <input type="text" id="search" name="search" value="Search" />
     </>
   );
 };
