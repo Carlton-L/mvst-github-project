f2cb50389fa2548ca5be8b48bcd129a756eedbae
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2023-12-31 01:31:07 +0100

    Search results page styling

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,5 +1,15 @@
 const SearchResults = () => {
-  return <div>Stuff</div>;
+  return (
+    <main className="flex flex-col items-center justify-start grow min-h-full">
+      <h3 className="flex mt-3.5 text-xl">
+        Users with &nbsp;
+        <span className="text-primary inline-block">{'"foo"'}</span>
+      </h3>
+      <ul>
+        <li className="mt-3"></li>
+      </ul>
+    </main>
+  );
 };
 
 export default SearchResults;
