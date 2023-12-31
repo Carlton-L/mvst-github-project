4c2704c11bb36581a02f9afcdaef13ab02952c5f
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2023-12-31 03:24:53 +0100

    added User search query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import Root from "./routes/Root.tsx";
 import ErrorPage from "./error-page.tsx";
 import SearchResults from "./pages/SearchResults.tsx";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
+
+const host = import.meta.env.VITE_API_HOST;
+const token = import.meta.env.VITE_API_TOKEN;
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: { ...headers, authorization: token ? `Bearer ${token}` : "" },
+  };
+});
+
+const httpLink = createHttpLink({ uri: host });
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
 
 const router = createBrowserRouter([
   {
@@ -22,6 +46,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
