20c74b777b115143252d648c92c4181ff4202f26
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2024-01-05 23:29:01 +0100

    Added loading spinner animation to SearchResults.tsx

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -35,7 +35,14 @@ const SearchResults = (): React.JSX.Element => {
   });
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex space-x-2 justify-center items-center bg-black h-screen">
+        <span className="sr-only">Loading...</span>
+        <div className="h-8 w-8 bg-primary rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-8 w-8 bg-primary rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-8 w-8 bg-primary rounded-full animate-bounce"></div>
+      </div>
+    );
   }
 
   if (error) {
